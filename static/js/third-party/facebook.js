5868117561cb8437ff3e925596552a49408b3420
Shashank Bhargava <14sbhargava@gmail.com>
2014-09-18 14:47:49 -0700

    update id

--- static/js/third-party/facebook.js
+++ static/js/third-party/facebook.js
@@ -32,7 +32,7 @@
 
   window.fbAsyncInit = function() {
   FB.init({
-    appId      : '368915006593397',
+    appId      : '370976186387279',
     cookie     : true,  // enable cookies to allow the server to access 
                         // the session
     xfbml      : true,  // parse social plugins on this page
